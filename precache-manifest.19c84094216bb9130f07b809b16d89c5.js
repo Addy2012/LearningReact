6de955433e30fbc006e4060c96dc6838ed26268c
Addy2012 <kumaranand287@gmail.com>
2020-05-01 12:48:51 +0530

    Updates

---
+++ precache-manifest.19c84094216bb9130f07b809b16d89c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d94aa14f7581c90992d56e921b65dc",
+    "url": "/LearningReact/index.html"
+  },
+  {
+    "revision": "485931db2a9d39034de6",
+    "url": "/LearningReact/static/css/main.c2347a36.chunk.css"
+  },
+  {
+    "revision": "e846d78b58077510b4da",
+    "url": "/LearningReact/static/js/2.8e272a29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LearningReact/static/js/2.8e272a29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485931db2a9d39034de6",
+    "url": "/LearningReact/static/js/main.fc3974d6.chunk.js"
+  },
+  {
+    "revision": "aefaf4b7078ce178778a",
+    "url": "/LearningReact/static/js/runtime-main.dd3bd5d0.js"
+  }
+]);
